5777f413e0880dd91ca2bcfe49f36d869cad174e
OsierRay <michunter2232@gmail.com>
2015-04-18 22:22:20 -0400

    added ajax for dynamic loading of content.

--- Website/js/dynamic.js
+++ Website/js/dynamic.js
@@ -1,2 +1,16 @@
 // this file will be used to create front end dynamic site
+var ourTeam = new XMLHttpRequest();
+ourTeam.onreadystatechange = function (){
+    if(ourTeam.onreadystatechange === 4){
+        document.getElementById('MainContent').innerHTML= ourTeam.responseText;
+
+    }
+
+};
+  ourTeam.open('GET','ourTeam.html');
+    function sendTeamPage(){
+
+        ourTeam.send();
+       // document.getElementById('ourTeam').style.display ="none";
+    };
 
