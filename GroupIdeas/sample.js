bdfaa48ee0956cd79338b3cd205653f133c623a6
OsierRay <michunter2232@gmail.com>
2015-04-20 17:12:46 -0400

    built a slider prototype added the idea behind it to the Ideas.txt file

---
+++ GroupIdeas/sample.js
@@ -0,0 +1,22 @@
+// on hover event to remove faded overlay from profile pic
+
+$('#').hover(function(){           // # replaced with selceted pic
+    $('#').removeClass("classname") //# replaced with selected css id // classname replaced with css class name to be removed from pic
+
+});
+
+// Bio section onclick event
+
+$('#').click(function(){    //#will be replaced with ID of the photo //
+    $('#').slideDown("slow",function(){  // this # will be replaced with the div that contains the bio
+
+       // complete the animaiton
+        //I left this function inplace in the even we need to manipulate dom elements to meet the design
+        $('selected element').addClass()
+         $('selected element').position()
+         $('selected element').removeClass()
+    });
+
+});
+
+// i will have a better idea of how to produce the desired effect when the html is in place
